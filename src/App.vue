8d6b7ad5d6c31a5f1540e42380badfa47ac6f8db
SAMEER GAIKWAD <sameergaikwad222@hotmail.com>
2020-12-09 07:39:59 +0530

    working timer

---
+++ src/App.vue
@@ -0,0 +1,130 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-container>
+        <v-switch v-model="clear" label="Clear All" color="red"></v-switch>
+        <v-switch v-model="set" label="Set All" color="green"></v-switch>
+      </v-container>
+      <v-divider inset></v-divider>
+      <v-container class="d-flex flex-column justify-space-around">
+        <v-switch v-model="showYear" color="teal" label="Year"></v-switch>
+        <v-switch v-model="showMonth" color="red" label="Month"></v-switch>
+        <v-switch v-model="showDay" color="purple" label="Day"></v-switch>
+        <v-switch v-model="showHour" color="green" label="Hour"></v-switch>
+        <v-switch v-model="showMinute" color="pink" label="Minute"></v-switch>
+        <v-switch v-model="showSecond" color="blue" label="Second"></v-switch>
+      </v-container>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="purple"
+      extended
+      src="https://picsum.photos/1920/1080?random"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Digital Clock</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <timer
+        class="mx-auto"
+        :showYear="showYear"
+        :showMonth="showMonth"
+        :showDay="showDay"
+        :showHour="showHour"
+        :showMinute="showMinute"
+        :showSecond="showSecond"
+      />
+    </v-main>
+    <v-btn
+      class="mx-2"
+      fab
+      dark
+      large
+      color="purple"
+      id="GitFloat"
+      @click="routeToMySite()"
+    >
+      <v-icon dark>
+        mdi-github
+      </v-icon>
+    </v-btn>
+  </v-app>
+</template>
+
+<script>
+import timer from "./components/timer";
+export default {
+  name: "App",
+
+  components: {
+    timer
+  },
+
+  data: () => ({
+    drawer: null,
+    clear: false,
+    set: false,
+    showYear: false,
+    showMonth: false,
+    showDay: false,
+    showHour: false,
+    showMinute: false,
+    showSecond: false
+  }),
+
+  methods: {
+    clearAll() {
+      this.showYear = false;
+      this.showMonth = false;
+      this.showDay = false;
+      this.showHour = false;
+      this.showMinute = false;
+      this.showSecond = false;
+    },
+    setAll() {
+      this.showYear = true;
+      this.showMonth = true;
+      this.showDay = true;
+      this.showHour = true;
+      this.showMinute = true;
+      this.showSecond = true;
+    },
+    routeToMySite() {
+      window.open("https://sameergaikwad.com", "_blank");
+    }
+  },
+  watch: {
+    clear(value) {
+      if (value) {
+        this.clearAll();
+        this.set = false;
+      }
+    },
+    set(value) {
+      if (value) {
+        this.setAll();
+        this.clear = false;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+body {
+  position: relative;
+}
+#GitFloat {
+  position: absolute;
+  bottom: 2.7%;
+  right: 2%;
+}
+</style>
---
+++ src/components/timer.vue
@@ -0,0 +1,170 @@
+<template>
+  <v-container class="fill-height mx-auto d-flex justify-space-around">
+    <ul class="list-group" v-sortable>
+      <li class="list-group-item border">Foo</li>
+      <li class="list-group-item border">Bar</li>
+      <li class="list-group-item border">Baz</li>
+    </ul>
+    <!-- Year Card -->
+    <v-hover v-slot="{ hover }" close-delay="200">
+      <v-card
+        v-if="showYear"
+        :elevation="hover ? 16 : 2"
+        :class="{ 'on-hover': hover }"
+        v-ripple
+        width="175px"
+        height="175px"
+        class="d-flex flex-column justify-center align-center"
+      >
+        <v-card-title>Year</v-card-title>
+        <v-divider inset></v-divider>
+        <v-card-text
+          class="text-lg-h2 text-md-h3 text-sm-h5 text-xl-h2 text-h3 text-center"
+        >
+          {{ year }}
+        </v-card-text>
+      </v-card>
+    </v-hover>
+    <!-- Month Card -->
+    <v-hover v-slot="{ hover }" close-delay="200">
+      <v-card
+        v-if="showMonth"
+        :elevation="hover ? 16 : 2"
+        :class="{ 'on-hover': hover }"
+        v-ripple
+        width="175px"
+        height="175px"
+        class="d-flex flex-column justify-center align-center"
+      >
+        <v-card-title>Month</v-card-title>
+        <v-divider inset></v-divider>
+        <v-card-text
+          class="text-lg-h2 text-md-h3 text-sm-h5 text-xl-h2 text-h3 text-center"
+        >
+          {{ month }}
+        </v-card-text>
+      </v-card>
+    </v-hover>
+    <!-- Day Card -->
+    <v-hover v-slot="{ hover }" close-delay="200">
+      <v-card
+        v-if="showDay"
+        :elevation="hover ? 16 : 2"
+        :class="{ 'on-hover': hover }"
+        v-ripple
+        width="175px"
+        height="175px"
+        class="d-flex flex-column justify-center align-center"
+      >
+        <v-card-title>Day</v-card-title>
+        <v-divider inset></v-divider>
+        <v-card-text
+          class="text-lg-h2 text-md-h3 text-sm-h5 text-xl-h2 text-h3 text-center"
+        >
+          {{ day }}
+        </v-card-text>
+      </v-card>
+    </v-hover>
+    <!-- Hour Card -->
+    <v-hover v-slot="{ hover }" close-delay="200">
+      <v-card
+        v-if="showHour"
+        :elevation="hover ? 16 : 2"
+        :class="{ 'on-hover': hover }"
+        v-ripple
+        width="175px"
+        height="175px"
+        class="d-flex flex-column justify-center align-center"
+      >
+        <v-card-title>Hour</v-card-title>
+        <v-divider inset></v-divider>
+        <v-card-text
+          class="text-lg-h2 text-md-h3 text-sm-h5 text-xl-h2 text-h3 text-center"
+        >
+          {{ hour }}
+        </v-card-text>
+      </v-card>
+    </v-hover>
+    <!-- Minute Card -->
+    <v-hover v-slot="{ hover }" close-delay="200">
+      <v-card
+        v-if="showMinute"
+        :elevation="hover ? 16 : 2"
+        :class="{ 'on-hover': hover }"
+        v-ripple
+        width="175px"
+        height="175px"
+        class="d-flex flex-column justify-center align-center"
+      >
+        <v-card-title>Minute</v-card-title>
+        <v-divider inset></v-divider>
+        <v-card-text
+          class="text-lg-h2 text-md-h3 text-sm-h5 text-xl-h2 text-h3 text-center"
+        >
+          {{ minute }}
+        </v-card-text>
+      </v-card>
+    </v-hover>
+    <!-- Second Card -->
+    <v-hover v-slot="{ hover }" close-delay="200">
+      <v-card
+        v-if="showSecond"
+        :elevation="hover ? 16 : 2"
+        :class="{ 'on-hover': hover }"
+        v-ripple
+        width="175px"
+        height="175px"
+        class="d-flex flex-column justify-center align-center"
+      >
+        <v-card-title>Second</v-card-title>
+        <v-divider inset></v-divider>
+        <v-card-text
+          class="text-lg-h2 text-md-h3 text-sm-h5 text-xl-h2 text-h3 text-center"
+        >
+          {{ second }}
+        </v-card-text>
+      </v-card>
+    </v-hover>
+  </v-container>
+</template>
+
+<script>
+import dayjs from "dayjs";
+export default {
+  props: {
+    showYear: Boolean,
+    showMonth: Boolean,
+    showDay: Boolean,
+    showHour: Boolean,
+    showMinute: Boolean,
+    showSecond: Boolean
+  },
+  data() {
+    return {
+      year: "2020",
+      month: "Dec",
+      day: "09",
+      hour: "17",
+      minute: "47",
+      second: "00"
+    };
+  },
+  methods: {
+    timer() {
+      var a = dayjs();
+      this.year = a.format("YYYY");
+      this.month = a.format("MMM");
+      this.day = a.format("DD");
+      this.hour = a.format("HH");
+      this.minute = a.format("mm");
+      this.second = a.format("ss");
+    }
+  },
+  mounted() {
+    this.timer();
+    setInterval(this.timer, 1000);
+  }
+};
+</script>
+
+<style></style>
