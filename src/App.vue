8cbb1d73167391d31bc2d385c1714ef5195456d9
SAMEER GAIKWAD <sameergaikwad222@hotmail.com>
2020-12-09 11:27:03 +0530

    app bar color made white

--- src/App.vue
+++ src/App.vue
@@ -28,8 +28,11 @@
           gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
         ></v-img>
       </template>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Digital Clock</v-toolbar-title>
+      <v-app-bar-nav-icon
+        @click="drawer = !drawer"
+        color="white"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title class="white--text">Digital Clock</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
